b3988b5e780b95c4fa2266d0af85bd5fe44e5429
OcularBear8 <130770129+OcularBear8@users.noreply.github.com>
2023-11-09 14:29:38 -0600

    Corrected LED flash timing

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,6 +1,6 @@
 # Imports
 import time
-from threading import Thread
+from threading import Thread, Event
 from bell.avr.mqtt.client import MQTTModule
 from bell.avr.mqtt.payloads import AvrApriltagsRawPayload
 
@@ -13,6 +13,7 @@ from loguru import logger
 class Sandbox(MQTTModule):
     def __init__(self) -> None:
         super().__init__()
+        self.flash_event = Event()
         #                  message that the | method that runs
         #                  code is          | when message is received
         #                  listening for    |
@@ -22,22 +23,17 @@ class Sandbox(MQTTModule):
         # Flashes green, blue, green on AprilTag 0 (Total duration 0.75 seconds)
         if payload["tags"][0]["id"] == 0:
             self.flash_led([0, 0, 255, 0], 0.25)
-            print("Flashing green...")
-            time.sleep(0.25)
+            self.flash_event.wait(0.25)
             self.flash_led([0, 0, 0, 255], 0.25)
-            print("Flashing blue...")
-            time.sleep(0.25)
+            self.flash_event.wait(0.25)
             self.flash_led([0, 0, 255, 0], 0.25)
-            print("Flashing green...")
-            time.sleep(0.25)
         # Flashed red 3 times on AprilTag 4/5/6 (Total duration 0.75 seconds)
         if payload["tags"][0]["id"] == 4 or payload["tags"][0]["id"] == 5 or payload["tags"][0]["id"] == 6:
             for _ in range(3):
-                print("Flashing red...")
                 self.flash_led([255, 0, 0, 0], 0.125)
-                time.sleep(0.125)
+                self.flash_event.wait(0.125)
                 self.flash_led([0, 0, 0, 0], 0.125)
-                time.sleep(0.125)
+                self.flash_event.wait(0.125)
 
     def flash_led(self, color: list, duration: float) -> None:
         # Colors LED temporarily
