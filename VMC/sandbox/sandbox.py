09df94b3ec8323356b1cb446fa292445fe648e48
OcularBear8 <130770129+OcularBear8@users.noreply.github.com>
2023-11-10 09:55:25 -0600

    Apriltag Timing

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,6 +1,5 @@
 # Imports
 import time
-from threading import Thread, Event
 from bell.avr.mqtt.client import MQTTModule
 from bell.avr.mqtt.payloads import AvrApriltagsRawPayload
 
@@ -13,7 +12,12 @@ from loguru import logger
 class Sandbox(MQTTModule):
     def __init__(self) -> None:
         super().__init__()
-        self.flash_event = Event()
+        self.recon1 = False
+        self.recon2 = False
+        self.recon3 = False
+        self.hotspot1 = False
+        self.hotspot2 = False
+        self.hotspot3 = False
         #                  message that the | method that runs
         #                  code is          | when message is received
         #                  listening for    |
@@ -78,24 +82,30 @@ class Sandbox(MQTTModule):
         )
 
     def handle_apriltag(self, payload: AvrApriltagsRawPayload) -> None:
-        payload = AvrApriltagsRawPayload
-        try:
-            id = payload["tags"][0]["id"]
-        except Exception as e:
-            print(e)
-            id = -1
         # Flashes green, blue, green on AprilTag 0 (Total duration 0.625 seconds)
         if id == 0:
-            self.flash_led([0, 0, 255, 0], 0.125)
-            self.flash_event.wait(0.125)
-            self.flash_led([0, 0, 0, 255], 0.125)
-            self.flash_event.wait(0.125)
-            self.flash_led([0, 0, 255, 0], 0.125)
+            current_time = time.time()
+            if not self.recon1:
+                self.flash_led([0, 0, 255, 0], 0.25)
+                self.recon1 = True
+            if time.time() - current_time > 0.25 and not self.recon2:
+                self.flash_led([0, 0, 0, 255], 0.25)
+                self.recon2 = True
+            if time.time() - current_time > 0.5 and not self.recon3:
+                self.flash_led([0, 0, 255, 0], 0.25)
+                self.recon3 = True
         # Flashed red 3 times on AprilTag 4/5/6 (Total duration 0.75 seconds)
         if id == 4 or id == 5 or id == 6:
-            for _ in range(3):
-                self.flash_event.wait(0.125)
-                self.flash_led([255, 0, 0, 0], 0.125)
+            current_time = time.time()
+            if not self.hotspot1:
+                self.flash_led([0, 255, 0, 0], 0.125)
+                self.hotspot1 = True
+            if time.time() - current_time > 0.25 and not self.hotspot2:
+                self.flash_led([0, 255, 0, 0], 0.125)
+                self.hotspot2 = True
+            if time.time() - current_time > 0.5 and not self.hotspot3:
+                self.flash_led([0, 255, 0, 0], 0.125)
+                self.hotspot3 = True
 
     def flash_led(self, color: list, duration: float) -> None:
         # Colors LED temporarily
@@ -106,10 +116,5 @@ class Sandbox(MQTTModule):
 
 if __name__ == "__main__":
     box = Sandbox()
-    # Threading methods makes sure that time.sleep() doesn't make the whole code just stop running.
-    # Separates the method's timing system from the rest of the code.
-    apriltag_thread = Thread(target=box.handle_apriltag, args=(AvrApriltagsRawPayload,))
-    apriltag_thread.setDaemon(True)
-    apriltag_thread.start()
     # Run method lets sandbox listen for MQTT messages
     box.run()
